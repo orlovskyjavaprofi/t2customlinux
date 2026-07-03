820eff264d1d51793baa2daf928b8a11c0269038
Alexander Orlovsky <javaprofi@gmx.de>
2026-07-03 17:32:22 +0200

    clean up

--- ExternalServersConfigs/ftpusersetup.sh
+++ ExternalServersConfigs/ftpusersetup.sh
@@ -4,7 +4,7 @@
 USER="filehoster"
 PASS="temporarypassword" # Replace with your desired throwaway password
 FTP_DIR="/mnt/t2-trunk/ftp"
-
+FTP_DIR2="/home/filehoster/ftp"
 # 1. Create the user if it doesn't exist
 if ! id "$USER" &>/dev/null; then
     useradd -m "$USER"
@@ -17,11 +17,16 @@ fi
 # 2. Setup the FTP directory
 # If you want to use the home dir instead, change FTP_DIR to "/home/$USER/ftp"
 mkdir -p "$FTP_DIR"
+mkdir -p "$FTP_DIR2"
 
 # 3. Apply permissions (nobody:nogroup is standard for restricted FTP)
 chown nobody:nogroup "$FTP_DIR"
 chmod a-w "$FTP_DIR"
 
+chown nobody:nogroup "$FTP_DIR2"
+chmod a-w "$FTP_DIR2"
+
 # 4. Verify
 echo "FTP directory setup complete at $FTP_DIR"
 ls -la "$FTP_DIR"
+ls -la "$FTP_DIR2"
