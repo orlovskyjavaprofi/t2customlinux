41ff5bd1d16bca7ddff511759ba40e92ca957d57
Alexander Orlovsky <javaprofi@gmx.de>
2026-07-19 02:39:04 +0200

    add simple pages

---
+++ docs/t2customlinux/_site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to custom to T2 Linux github repo build by Orlovsky Consulting GbR",
+        "excerpt":"CUSTOM T2 LINUX BUILD BY ORLOVSKY CONSULTING GbR We are not official T2 Linux developers, we build our own custom version of T2 Linux based on official repo of T2 Linux! Read our code of ethics Custom configs and examples for T2 Linux , build target X86_64 AMD CPU platform....","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2026/07/19/customBuildT2Linux.html"
+      }]
